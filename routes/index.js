628157a34e945509828e2dc44ecb7643beda7d79
Lyon Yao <lyon_yao@163.com>
2016-09-05 16:35:09 +0800

    Add mysql & simple code

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	logger.info('Welcome visit index...');
+	var db = require('../db');
+	db.execQuery({sql:'select * from menus where parent_id=?',args:[-1],handler:function(results){
+		console.log(results);
+	}});
+	db.release({});
    	res.render('index', { title: 'Hello Hellow' ,pluginCss:[],pluginScript:[],layout: 'layout'});
 });
 router.get('/hellow',function(req,res,next){
