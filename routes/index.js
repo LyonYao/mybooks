606e16e4c029ff2a2770fa614406fef5f1e86831
wnyao <wnyao@raipeng.com>
2016-09-06 14:46:24 +0800

    Fix error

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ router.get('/', function(req, res, next) {
 	db.execQuery({sql:'select * from menus where parent_id=?',args:[-1],handler:function(results){
 		console.log(results);
 	}});
-	db.release({});
    	res.render('index', { title: 'Hello Hellow' ,pluginCss:[],pluginScript:[],layout: 'layout'});
 });
 router.get('/hellow',function(req,res,next){
