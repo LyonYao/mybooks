add6ba668493e5c33d0992850ab22338372cce46
wnyao <wnyao@raipeng.com>
2016-09-18 16:57:33 +0800

    Finish simple login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db');
+var crypto = require('crypto');
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
 	var db = require('../db');
@@ -10,11 +11,24 @@ router.get('/users', function(req, res, next) {
 	res.send('respond with a resource');
 });
 router.post('/login', function(req, res) {
-	var db = require('../db');
-	db.execQuery({sql:'select * from menus where parent_id=?',args:[-1],handler:function(results){
-		console.log(results);
+	
+	db.execQuery({sql:'select * from users where user_name=?',args:[req.param('username')],handler:function(results){
+		if(results&&results.length>0){
+			var existsUser=results[0];
+			console.log(crypto.createHmac('md5','mybooks').update(req.param('password')).digest('hex'));
+			if(existsUser.password==crypto.createHmac('md5','mybooks').update(req.param('password')).digest('hex')){
+				var loginUser={id:existsUser.id,userName:existsUser.user_name,nickName:existsUser.nick_name,isLogin:true};
+				req.session.loginUser=loginUser;
+				res.redirect('/');
+			}else{
+				req.flash('errorMessage', '密码错误!');
+				res.redirect('/login');
+			}
+		}else{
+			req.flash('errorMessage', '用户名不存在!');
+			res.redirect('/login');
+		}
 	}});
-	req.flash('errorMessage', 'Flash is back!');
-	res.redirect('/login');
+	
 });
 module.exports = router;
